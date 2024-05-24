ceeb4aaf66080dc49c99ff47294def37b621976a
Umeshhegde234 <76684740+Umeshhegde234@users.noreply.github.com>
2024-05-24 16:42:27 +0530

    Add files via upload

---
+++ RabbitMq.Common/Services/RabbitMqService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using RabbitMq.Common.Models;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMq.Common.Services
+{
+    public interface IRabbitMqService
+    {
+        IConnection CreateChannel();
+    }
+
+    public class RabbitMqService : IRabbitMqService
+    {
+        private readonly RabbitMqConfiguration _configuration;
+        public RabbitMqService(IOptions<RabbitMqConfiguration> options)
+        {
+            _configuration = options.Value;
+        }
+        public IConnection CreateChannel()
+        {
+            ConnectionFactory connection = new ConnectionFactory()
+            {
+                //UserName = "twlnacps", //_configuration.Username,
+                //Password = "ai4rMKIGhr-Q722am9_DgWhJcOst7mfH",//"rabbitmq@693", //_configuration.Password,
+                //HostName = "puffin.rmq2.cloudamqp.com", //_configuration.HostName
+                //VirtualHost = "twlnacps",
+                //Port = 5672,
+                Uri = new Uri("amqps://twlnacps:ai4rMKIGhr-Q722am9_DgWhJcOst7mfH@puffin.rmq2.cloudamqp.com/twlnacps")
+            };
+            connection.DispatchConsumersAsync = true;
+            var channel = connection.CreateConnection();
+            return channel;
+
+            //var factory = new ConnectionFactory
+            //{
+            //    Uri = new Uri("amqps://twlnacps:ai4rMKIGhr-Q722am9_DgWhJcOst7mfH@puffin.rmq2.cloudamqp.com/twlnacps")
+            //};
+        }
+    }
+}
